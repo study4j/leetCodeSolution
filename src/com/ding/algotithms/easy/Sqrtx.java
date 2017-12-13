062f1f242f72bd82325fec20be0ad8d850661cd3
niyou2le <18435156270@163.com>
2017-12-13 22:57:16 +0800

    69

--- src/com/ding/algotithms/easy/Sqrtx.java
+++ src/com/ding/algotithms/easy/Sqrtx.java
@@ -13,20 +13,26 @@ import java.util.Scanner;
 public class Sqrtx {
 
     public static void main(String[] args) {
-        System.out.println(new Sqrtx().getSqrt(2147395599));
+
+        System.out.println(new Sqrtx().getSqrt(88));
     }
 
 
     public Integer getSqrt(int x) {
+        int n = x / 2;
         if (x == 0) {
             return 0;
         }
-        int mid = (1 + x) / 2;
-        while (true) {
-            if (mid > x / mid) {
-                mid = (1 + mid) / 2;
-            }
+        if (x < 4) {
+            return 1;
+        } if (x < 6) {
+            return 2;
         }
+
+        do {
+            n--;
+        } while (n > (x / n));
+        return n;
     }
 
     /*public Integer getSqrt(int x) {
